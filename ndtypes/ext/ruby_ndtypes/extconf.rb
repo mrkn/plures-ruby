c73707ef7cc93846e6cb752f512322325cdecbda
Kenta Murata <mrkn@mrkn.jp>
2018-09-23 12:09:39 +0900

    Simplify console log

--- ndtypes/ext/ruby_ndtypes/extconf.rb
+++ ndtypes/ext/ruby_ndtypes/extconf.rb
@@ -14,6 +14,21 @@ SHA256 = {
   'xnd' => 'b0b09a8ed5a96600d5c9225dce1b0b7f52e556ba4cb87e4c564dcb9c2208efb1'
 }
 
+def check_message_for(m)
+  f = caller[0][/in `([^<].*)'$/, 1] and f << ": " #` for vim #'
+  m = "#{m}... "
+  message "%s", m
+  a = r = nil
+  Logging::postpone do
+    r = yield
+    a = (r ? "yes" : "no")
+    "#{f}#{m}-------------------- #{a}\n\n"
+  end
+  message "%s\n", a
+  Logging::message "--------------------\n\n"
+  r
+end
+
 def download(url, filename)
   FileUtils.mkdir_p(File.dirname(filename))
   open(filename, "wb") do |out|
@@ -32,42 +47,58 @@ end
 def checkout(libname, revision, dest_dir)
   url = "https://github.com/plures/#{libname}/archive/#{revision}.zip"
   filename = File.join(dest_dir, "#{libname}.zip")
-  unless File.file?(filename) && check_sha256(libname, filename)
-    download(url, filename)
+
+  check_message_for("downloading #{libname} revision #{revision}") do
+    unless File.file?(filename) && check_sha256(libname, filename)
+      download(url, filename)
+    end
+    if check_sha256(libname, filename)
+      true
+    else
+      $stderr.puts "SHA256 mismatch in #{libname}"
+      false
+    end
   end
-  unless check_sha256(libname, filename)
-    abort "sha256 mismatch in #{libname}-#{revision}"
-  end
-  system('unzip', '-xo', filename, '-d', dest_dir)
 end
 
-def build_library(libname, revision, src_dir)
-  prefix = File.expand_path("../../..", __FILE__)
-  includedir = File.join(prefix, 'include')
-  libdir = File.join(prefix, "lib")
+def build_library(libname, revision, src_dir, prefix)
   cc = RbConfig.expand("$(CC)")
   cpp = RbConfig.expand("$(CPP)")
   dirname = "#{libname}-#{revision}"
   Dir.chdir(File.join(src_dir, dirname)) do
     puts "Enter #{Dir.pwd}"
-    system "./configure --prefix='#{prefix}' CC='#{cc}' CPP='#{cpp}'"
-    system "make"
-    system "make check"
-    system "make install" or abort
+    system "./configure --prefix='#{prefix}' CC='#{cc}' CPP='#{cpp}'" or return false
+    system "make" or return false
+    system "make check" or return false
+    system "make install" or return false
   ensure
     puts "Leave #{Dir.pwd}"
   end
-  append_cppflags("-I#{includedir}")
-  append_ldflags("-Wl,-rpath #{libdir}")
-  $LIBPATH << libdir
-  $libs = append_library($libs, libname)
+  true
 end
 
-def download_and_build_library(libname)
+def check_build_library(libname, revision, src_dir)
+  prefix = File.expand_path("../../..", __FILE__)
+  includedir = File.join(prefix, 'include')
+  libdir = File.join(prefix, "lib")
+
+  success = check_message_for "building #{libname} " do
+    build_library(libname, revision, src_dir, prefix)
+  end
+
+  if success
+    append_cppflags("-I#{includedir}")
+    append_ldflags("-Wl,-rpath #{libdir}")
+    $LIBPATH << libdir
+    $libs = append_library($libs, libname)
+  end
+end
+
+def download_and_build_library(libname, headername=nil)
   vendor_dir = File.expand_path("../../../vendor", __FILE__)
   revision = LIBRARY_REVISIONS[libname]
-  checkout(libname, revision, vendor_dir)
-  build_library(libname, revision, vendor_dir)
+  checkout(libname, revision, vendor_dir) and
+    check_build_library(libname, revision, vendor_dir)
 end
 
 $INSTALLFILES = [
@@ -78,7 +109,7 @@ $INSTALLFILES = [
   dir_config(lib)
   found = find_library(lib, nil, "/home/sameer/gitrepos/plures-ruby/build/lib/")
   unless found
-    download_and_build_library(lib)
+    download_and_build_library(lib) or abort
     have_library(lib)
   end
 end
